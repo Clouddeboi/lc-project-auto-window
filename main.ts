9dcc8f19d7e14ffa9d3f26bd280a4860b15e33d1
Clouddeboi <93706431+Clouddeboi@users.noreply.github.com>
2022-01-17 19:03:11 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,30 @@ let temperature = 0
 pins.servoWritePin(AnalogPin.P1, 0)
 basic.forever(function () {
     basic.showNumber(input.temperature())
+    basic.pause(1000)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        pins.servoWritePin(AnalogPin.P1, 0)
+        basic.pause(5000)
+    }
+})
+basic.forever(function () {
+    basic.showNumber(input.temperature())
     while (true) {
         temperature = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P1)
         if (input.temperature() < 19) {
             pins.servoWritePin(AnalogPin.P1, 0)
             basic.pause(2000)
-        } else if (input.buttonIsPressed(Button.B)) {
-            pins.servoWritePin(AnalogPin.P1, 180)
-            basic.pause(2000)
-        } else if (input.buttonIsPressed(Button.A)) {
-            pins.servoWritePin(AnalogPin.P1, 0)
         } else {
-            pins.servoWritePin(AnalogPin.P1, 100)
+            pins.servoWritePin(AnalogPin.P1, 90)
             basic.pause(2000)
         }
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        pins.servoWritePin(AnalogPin.P1, 90)
+        basic.pause(5000)
     }
 })
